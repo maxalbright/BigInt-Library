287a478584dc0d21a0256cec76a93bd4d42141a6
Max Albright <mdalbrig@usc.edu>
2020-12-06 16:13:38 -0600

    Update personaltest.cpp

--- personaltest.cpp
+++ personaltest.cpp
@@ -12,9 +12,15 @@ int main (int argc, char* argv[]) {
 	BigInt b(-32, 10);
 	a-=b;
 	for (int i = 0; i < 10; i++) {
-		a %= b;
+		a /= b;
 		cout << a.to_string() << endl;
 	}
+	BigInt c("8C12A4", 16);
+	BigInt d("B", 16);
+	
+	c.exponentiation(d);
+	
+	cout << c.to_string() << endl;
 
 	return 0;
 }
