6048297fdcd9cee69509f075195c88b386f2fa3e
Max Albright <mdalbrig@usc.edu>
2020-12-06 16:05:17 -0600

    Update personaltest.cpp

--- personaltest.cpp
+++ personaltest.cpp
@@ -5,13 +5,16 @@
 
 using namespace std;
 
+//This is just a file to test the operators
+
 int main (int argc, char* argv[]) {
 	BigInt a(123134, 10);
 	BigInt b(-32, 10);
 	a-=b;
-	cout << a.to_string() << endl;
-
-
+	for (int i = 0; i < 10; i++) {
+		a %= b;
+		cout << a.to_string() << endl;
+	}
 
 	return 0;
 }
